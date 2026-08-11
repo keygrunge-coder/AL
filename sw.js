5577127b66c6e30cef402a6538e36bf15548644a
rckwd <key.grunge@gmail.com>
2026-08-11 19:42:23 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = 'komando-packing-cache-v1';
+const CACHE_NAME = 'intan-scanner-cache-v2'; // Naikkan versi cache biar ke-refresh
 const assets = [
-  'index.html',
+  'scanretur.html', // Ubah index.html jadi halaman utama lo
+  'icon.png',       // Masukin icon baru lo ke cache
   'https://unpkg.com/html5-qrcode'
 ];
 
@@ -8,6 +9,21 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    })
+  );
+});
+
+// Hapus cache lama kalau ada update versi baru
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
